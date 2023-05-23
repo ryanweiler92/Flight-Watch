4d8655e9cd229ba883b45596496e31e4336c0a28
ryanweiler92 <ryanweiler92@gmail.com>
2023-05-23 13:35:20 -0400

    chakra ui provider

--- src/redux/provider.tsx
+++ src/redux/provider.tsx
@@ -1,8 +1,19 @@
 "use client";
 
-import { store } from "./store";
 import { Provider } from "react-redux";
+import { ChakraProvider } from "@chakra-ui/react";
+import { CacheProvider } from '@chakra-ui/next-js'
+import { store } from "./store";
+
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <Provider store={store}>{children}</Provider>;
+  return (
+  <Provider store={store}>
+    <CacheProvider>
+      <ChakraProvider>
+        {children}
+      </ChakraProvider>
+    </CacheProvider>
+    </Provider>
+    );
 }
