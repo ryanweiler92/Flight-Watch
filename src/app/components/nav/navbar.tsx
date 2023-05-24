4afa08678dc081abee7a9d78af55911546886054
ryanweiler92 <ryanweiler92@gmail.com>
2023-05-23 21:54:33 -0400

    chakra ui integration + dashboard navbar

--- src/app/components/nav/navbar.tsx
+++ src/app/components/nav/navbar.tsx
@@ -1,9 +1,125 @@
 "use client";
 
 import Image from "next/image";
+import React from "react";
+
+import {
+  chakra,
+  Box,
+  Flex,
+  useColorModeValue,
+  VisuallyHidden,
+  HStack,
+  Button,
+  useDisclosure,
+  VStack,
+  IconButton,
+  CloseButton,
+} from "@chakra-ui/react";
+import { AiOutlineMenu } from "react-icons/ai";
+import { Logo } from "@choc-ui/logo";
 
 const Navbar = () => {
-  return <></>;
+  const bg = useColorModeValue("white", "gray.800");
+  const mobileNav = useDisclosure();
+
+  return (
+    <React.Fragment>
+      <chakra.header
+        bg={"brand.800"}
+        w="full"
+        px={{ base: 2, sm: 4 }}
+        py={4}
+        shadow="md"
+      >
+        <Flex alignItems="center" justifyContent="space-between" mx="auto">
+          <Flex>
+            <chakra.a
+              href="/"
+              title="Flight-Watch Dashboard"
+              display="flex"
+              alignItems="center"
+            >
+              <Logo />
+              <VisuallyHidden>Flight-Watch</VisuallyHidden>
+            </chakra.a>
+            <chakra.h1
+              fontSize="xl"
+              fontWeight="medium"
+              ml="2"
+              color={"whiteAlpha.900"}
+            >
+              Flight-Watch
+            </chakra.h1>
+          </Flex>
+          <HStack display="flex" alignItems="center" spacing={1}>
+            <HStack
+              spacing={1}
+              mr={1}
+              color="whiteAlpha.900"
+              display={{ base: "none", md: "inline-flex" }}
+            >
+              <Button variant="ghost" _hover={{ bg: "brand.300" }}>
+                Home
+              </Button>
+              <Button variant="ghost" _hover={{ bg: "brand.300" }}>
+                Dashboard
+              </Button>
+              <Button variant="ghost" _hover={{ bg: "brand.300" }}>
+                Sign in
+              </Button>
+            </HStack>
+            <Button colorScheme="brand" size="sm">
+              Get Started
+            </Button>
+            <Box display={{ base: "inline-flex", md: "none" }}>
+              <IconButton
+                display={{ base: "flex", md: "none" }}
+                aria-label="Open menu"
+                fontSize="20px"
+                color="gray.800"
+                _dark={{ color: "inherit" }}
+                variant="ghost"
+                icon={<AiOutlineMenu />}
+                onClick={mobileNav.onOpen}
+              />
+
+              <VStack
+                pos="absolute"
+                top={0}
+                left={0}
+                right={0}
+                display={mobileNav.isOpen ? "flex" : "none"}
+                flexDirection="column"
+                p={2}
+                pb={4}
+                m={2}
+                bg={bg}
+                spacing={3}
+                rounded="sm"
+                shadow="sm"
+              >
+                <CloseButton
+                  aria-label="Close menu"
+                  onClick={mobileNav.onClose}
+                />
+
+                <Button w="full" variant="ghost">
+                  Home
+                </Button>
+                <Button w="full" variant="ghost">
+                  Dashboard
+                </Button>
+                <Button w="full" variant="ghost">
+                  Sign in
+                </Button>
+              </VStack>
+            </Box>
+          </HStack>
+        </Flex>
+      </chakra.header>
+    </React.Fragment>
+  );
 };
 
 export default Navbar;
--- src/redux/provider.tsx
+++ src/redux/provider.tsx
@@ -1,19 +1,37 @@
 "use client";
 
 import { Provider } from "react-redux";
-import { ChakraProvider } from "@chakra-ui/react";
-import { CacheProvider } from '@chakra-ui/next-js'
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { CacheProvider } from "@chakra-ui/next-js";
 import { store } from "./store";
 
-
 export function Providers({ children }: { children: React.ReactNode }) {
+  const colors = {
+    brand: {
+      50: "#ecefff",
+      100: "#cbceeb",
+      200: "#a9aed6",
+      300: "#888ec5",
+      400: "#666db3",
+      500: "#4d5499",
+      600: "#3c4178",
+      700: "#2a2f57",
+      800: "#181c37",
+      900: "#080819",
+    },
+  };
+  const config = {
+    initialColorMode: "dark",
+    useSystemColorMode: false,
+  };
+
+  const theme = extendTheme({ colors, config });
+
   return (
-  <Provider store={store}>
-    <CacheProvider>
-      <ChakraProvider>
-        {children}
-      </ChakraProvider>
-    </CacheProvider>
+    <Provider store={store}>
+      <CacheProvider>
+        <ChakraProvider theme={theme}>{children}</ChakraProvider>
+      </CacheProvider>
     </Provider>
-    );
+  );
 }
